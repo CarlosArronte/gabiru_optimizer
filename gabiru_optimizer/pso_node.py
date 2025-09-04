ac5cb85cff9638af1a4cc610ba6aa69473b41e45
CarlosArronte <carronte94@gmail.com>
2025-09-04 15:46:40 -0300

    Refactor PSOOptimizerNode to initialize subscriptions and services in the constructor, and improve message publishing logic

--- gabiru_optimizer/pso_node.py
+++ gabiru_optimizer/pso_node.py
@@ -15,22 +15,26 @@ CSV_PATH = "optimized_PP_parms.csv"
 class PSOOptimizerNode(Node):
     def __init__(self):
         super().__init__('pso_optimizer_node')
-        qos = QoSProfile(
-            reliability=QoSReliabilityPolicy.RELIABLE,
-            history=QoSHistoryPolicy.KEEP_ALL,
-            depth=1000
-        )
-        self.subscription = self.create_subscription(
-            String,
-            'gabiru/segments',
-            self.segment_callback,
-            qos)
-        self.publisher = self.create_publisher(String, 'gabiru/params', qos)
-        self.optimized_segments = set()
-    
-    def save_to_csv(self,segment_id, Ld, vd, w_max):
-        save_pp_params(segment_id, Ld, vd, w_max)
 
+        self.srv = self.create_service(Trigger,'ready_to_recive_optimization',self.ready_callback)#1
+        self.client = self.create_client(Trigger, 'start_optimization')      
+        self.publisher = self.create_publisher(String, 'gabiru/params', 10)
+        self.optimized_segments = set()
+
+        self.optim_msg = String()
+    
+    def ready_callback(self,request, response):
+        self.subscription = self.create_subscription(#2
+                String,
+                'gabiru/segments',
+                self.segment_callback,
+                10
+            )
+        response.success = True
+        response.message = "PathOptimizer listo para recibir datos"
+        self.get_logger().info("Servicio ready_to_optimize: Suscripción a /processed_data activada")
+        return response 
+    
     def segment_callback(self, msg):
         self.get_logger().info(f"Mensaje recibido: {msg.data}")
 
@@ -65,18 +69,46 @@ class PSOOptimizerNode(Node):
             self.save_to_csv(segment_id, Ld, vd, w_max)
 
 
-            msg_out = String()
-            msg_out.data = json.dumps({
+            self.optim_msg.data = json.dumps({
                 "segment_id": segment_id,
                 "Ld": Ld,
                 "vd": vd,
                 "w_max": w_max
             })
-            self.publisher.publish(msg_out)
-            self.get_logger().info(f"Publicado parámetros para segmento {segment_id}: {msg_out.data}")
-            self.optimized_segments.add(segment_id)
+
+            self.wait_for_publish()  
+            
+            
         else:
             self.get_logger().warning(f"No se pudo optimizar el segmento {segment_id}.")
+
+    def wait_for_publish(self):
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Esperando por modulo PP")
+        request = Trigger.Request()
+        future = self.client.call_async(request)
+        future.add_done_callback(self.ready_response_callback)    
+    
+
+    def ready_response_callback(self,future):
+        try:
+            response = future.result()
+            if response.success:
+                optim_dict = json.loads(self.optim_msg.data)
+                self.get_logger().info(f"PathProcessor listo: {response.message}")
+                self.publisher.publish(self.optim_msg)
+                self.get_logger().info(f"Publicado parámetros para segmento {optim_dict['segment_id']}: {self.optim_msg.data}")
+                self.optimized_segments.add(optim_dict['segment_id'])
+            else:
+                self.get_logger().error(f"Fallo en la preparación: {response.message}")
+        except Exception as e:
+            self.get_logger().error(f"Error al llamar al servicio: {e}")
+
+
+    def save_to_csv(self,segment_id, Ld, vd, w_max):
+        save_pp_params(segment_id, Ld, vd, w_max)
+
+ 
 
 def main(args=None):
     rclpy.init(args=args)
